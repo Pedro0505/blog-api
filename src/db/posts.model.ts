e799ca56a54d7e054df7ae62732c80b431b02f46
pedro <pedrohrds05@hotmail.com>
2023-06-15 14:17:06 -0300

    feat: adding posts model

---
+++ src/db/posts.model.ts
@@ -0,0 +1,21 @@
+import * as mongoose from 'mongoose';
+
+export const PostsSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    content: { type: String, required: true },
+    published: { type: String, required: true },
+    category: { type: String, required: true },
+  },
+  { versionKey: false },
+);
+
+export interface Posts {
+  id: string;
+  title: string;
+  description: string;
+  content: string;
+  published: string;
+  category: string;
+}
