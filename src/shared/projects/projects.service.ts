3d37d462e585cb689043d1a7871568b6c143381b
pedro <pedrohrds05@hotmail.com>
2023-06-19 10:01:35 -0300

    feat: adding CRUD in project service

--- src/shared/projects/projects.service.ts
+++ src/shared/projects/projects.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { ProjectsRepository } from './projects.repository';
+import IProjectBodyCreate from './interfaces/IProjectBodyCreate';
+import IProjectBodyUpdate from './interfaces/IProjectBodyUpdate';
 
 @Injectable()
 export class ProjectsService {
   constructor(private projectsRepository: ProjectsRepository) {}
 
-  public async getAll() {
-    return await this.projectsRepository.getAll();
+  public async getAllProjects() {
+    return await this.projectsRepository.getAllProjects();
+  }
+
+  public async createProject(project: IProjectBodyCreate) {
+    return await this.projectsRepository.createProject(project);
+  }
+
+  public async deleteProjectBydId(id: string) {
+    await this.projectsRepository.deleteProjectBydId(id);
+  }
+
+  public async updateProjectById(project: IProjectBodyUpdate, id: string) {
+    return await this.projectsRepository.updateProjectById(project, id);
   }
 }
