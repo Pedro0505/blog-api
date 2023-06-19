232585c56204ec98a9b8d7564a48d971ae6bdc38
pedro <pedrohrds05@hotmail.com>
2023-06-19 09:20:28 -0300

    feat: adding CRUD in post service

--- src/shared/posts/posts.service.ts
+++ src/shared/posts/posts.service.ts
@@ -1,11 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { PostsRepository } from './posts.repository';
+import IPostBodyCreate from './interfaces/IPostBodyCreate';
+import IPostBodyUpdate from './interfaces/IPostBodyUpdate';
 
 @Injectable()
 export class PostsService {
   constructor(private postsRepository: PostsRepository) {}
 
-  public async getAll() {
-    return await this.postsRepository.getAll();
+  public async getAllPosts() {
+    return await this.postsRepository.getAllPosts();
+  }
+
+  public async getPostById(id: string) {
+    return await this.postsRepository.getPostById(id);
+  }
+
+  public async createPost(post: IPostBodyCreate) {
+    const newPost = { ...post, published: new Date().toISOString() };
+
+    return await this.postsRepository.createPost(newPost);
+  }
+
+  public async deletePostBydId(id: string) {
+    await this.postsRepository.deletePostBydId(id);
+  }
+
+  public async updatePostById(body: IPostBodyUpdate, id: string) {
+    return await this.postsRepository.updatePostById(body, id);
   }
 }
