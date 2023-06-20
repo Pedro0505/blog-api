dcb1e825e95b4efdc8e47ff85b312fc4c6180c1e
pedro <pedrohrds05@hotmail.com>
2023-06-20 10:11:59 -0300

    feat: adding data to seed

---
+++ src/db/seeders/data/posts.data.ts
@@ -0,0 +1,26 @@
+export default [
+  {
+    title: 'Best post of the year',
+    category: 'Docker',
+    content:
+      '<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, deserunt mollit anim id est laborum. </p>',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, deserunt mollit anim id est laborum.',
+  },
+  {
+    title: 'Best post of the month',
+    category: 'Node',
+    content:
+      '<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, deserunt mollit anim id est laborum. </p>',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, deserunt mollit anim id est laborum.',
+  },
+  {
+    title: 'Best post of the day',
+    category: 'RabbitMq',
+    content:
+      '<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, deserunt mollit anim id est laborum. </p>',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, deserunt mollit anim id est laborum.',
+  },
+];
