4e46e8c757e46387d06e857e15841ae7652db4da
pedro <pedrohrds05@hotmail.com>
2023-06-28 13:42:05 -0300

    test: removing id from data in project mock

--- test/mock/data/project.ts
+++ test/mock/data/project.ts
@@ -1,13 +1,11 @@
 export default {
   projects: [
     {
-      id: '6491cd4543f10f90bb09ed7d',
       name: 'Projeto 1',
       url: 'https://projeto1.com',
       description: 'Good',
     },
     {
-      id: '6491cd4543f10f90bb09ed7c',
       name: 'Projeto 2',
       url: 'https://projeto2.com',
       description: 'Good',
@@ -21,7 +19,6 @@ export default {
   },
 
   projectCreated: {
-    id: '6492e27a5b64aa312860b247',
     name: 'Projeto 3',
     url: 'https://projeto3.com',
     description: 'Good',
@@ -32,7 +29,6 @@ export default {
   },
 
   projectUpdated: {
-    id: '6491cd4543f10f90bb09ed7d',
     name: 'Projeto 25',
     url: 'https://projeto1.com',
     description: 'Good',
