0fff3da45a33183dab8de498eee19d798fea81d8
pedro <pedrohrds05@hotmail.com>
2023-07-02 07:41:35 -0300

    test: adding users test

--- test/mock/data/index.ts
+++ test/mock/data/index.ts
@@ -1,5 +1,6 @@
 import projectsMock from './project';
 import postsMock from './posts';
 import authMock from './auth';
+import userMock from './user';
 
-export { projectsMock, postsMock, authMock };
+export { projectsMock, postsMock, authMock, userMock };
---
+++ test/mock/data/user.ts
@@ -0,0 +1,6 @@
+export default {
+  user: {
+    username: 'Jonh Doe',
+    password: '$2a$10$NpwNY8SZj7BJOGLHLFes0eM8IefogNDuuKbx8X0XVklxtuT988nQi', //minhaIncrivelSenha1
+  },
+};
